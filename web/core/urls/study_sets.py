a691e849cb8e4a5c107a03f0cb61831f756fece5
ernestomgz <109539998+ernestomgz@users.noreply.github.com>
2026-04-01 19:10:48 +0200

    edit custom sets

--- web/core/urls/study_sets.py
+++ web/core/urls/study_sets.py
@@ -6,6 +6,7 @@ app_name = 'study_sets'
 
 urlpatterns = [
     path('create/', study_sets.study_set_create, name='create'),
+    path('<int:pk>/edit/', study_sets.study_set_edit, name='edit'),
     path('<int:pk>/delete/', study_sets.study_set_delete, name='delete'),
     path('<int:pk>/pin/', study_sets.study_set_toggle_pin, name='toggle_pin'),
     path('decks/<int:deck_id>/toggle/', study_sets.study_set_toggle_deck, name='toggle_deck'),
